bf954c67ad533cded4da89f4428c1d1c48c44a48
Асадбек* <asadbekme2002@gmail.com>
2023-02-22 21:54:03 +0500

    in progress

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,35 @@
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import { sliderImgs } from '../../assets/images';
+import './Slider.scss';
+
 const Slider = () => {
+  let settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
-    <div>Slider</div>
+    <section className='slider'>
+      <div className='container'>
+        <div className='slider-content overflow-x-hidden'>
+          <Slider {...settings}>
+            {sliderImgs.map((_, i) => (
+              <div className='slider-item'>
+                <img src={sliderImgs[i]} alt="slider image" />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sidebarReducer from '../features/sidebar/sidebarSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    sidebar: sidebarReducer,
+  },
   devTools: process.env.NODE_ENV !== 'production'
 });
